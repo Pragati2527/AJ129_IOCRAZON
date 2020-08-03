a82e79f37737d32d7dbe17b45986c915f0bbeb73
amir1189 <52484530+amir1189@users.noreply.github.com>
2020-08-03 15:30:59 +0530

    Update DENSITY.ino

--- DENSITY.ino
+++ DENSITY.ino
@@ -1,222 +1,179 @@
-#include "FS.h"
-#include <ESP8266WiFi.h>
-#include <PubSubClient.h>
-#include <NTPClient.h>
-#include <WiFiUdp.h>
-#include "HX711.h"
+from flask import Flask, render_template, request
+#import json
+import boto3
+from boto3.dynamodb.conditions import Key
+from decimal import Decimal
+import uuid
+import smtplib
+import mysql.connector
+from flask import jsonify
+
+from flask_cors import CORS, cross_origin
 
 
-#define calibration_factor -235000.0
+i=0
+dynamodb = boto3.resource('dynamodb')
+petrol_table = dynamodb.Table('petrol_data')
 
-#define DOUT  D7
-#define CLK  D8
-const int buttonPin = D0;
-int buttonState = 0;
+mydb = mysql.connector.connect(
+   host='127.0.0.1', user='root', password='', database='petrol')
+mycursor = mydb.cursor()
 
-HX711 scale;
 
-const char* ssid = "MY-ASUS";
-const char* password = "amiramir";
 
-WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP, "pool.ntp.org");
+app = Flask(__name__)
+CORS(app)
 
-const char* AWS_endpoint = "a1kzk0bz9yqvf0-ats.iot.ap-south-1.amazonaws.com"; //MQTT broker ip
+# def float_range(start, stop, step):
+#     while start < stop:
+#         yield float(round(start,1))
+#         start += float(step)
+#     if start!=stop:
+#         yield float(round(stop))
 
-void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
-  for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
-  }
-  Serial.println();
+# n = float(input("Enter N : "))
 
-}
-WiFiClientSecure espClient;
-PubSubClient client(AWS_endpoint, 8883, callback, espClient); //set MQTT port number to 8883 as per //standard
-//============================================================================
-#define BUFFER_LEN 256
-long lastMsg = 0;
-char msg[BUFFER_LEN];
-int value = 0;
-byte mac[6];
-char mac_Id[18];
-//============================================================================
+# seq = list(float_range(0, n, 0.4))
 
-void setup_wifi() {
+# def json_data(i):
+#     return json.dumps({'progress': i })
 
-  delay(10);
-  // We start by connecting to a WiFi network
-  espClient.setBufferSizes(512, 512);
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
+# json_list = list(map(json_data,seq))
 
-  WiFi.begin(ssid, password);
 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
+def sendEmail(email,date,am,tid,vid):
+    rec_email =email
+    sender_email = "piyushait12@gmail.com"
+    password ="qazmlp10"
+    message= " fuel Quantity - "+am+" L\n"+"Date : -"+date+"\n"+"Transactio ID: "+tid+"\n"+"Vendor Id:"+vid
 
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
+    m="""
+    Subject: Fuel verification""" + message
 
-  timeClient.begin();
-  while (!timeClient.update()) {
-    timeClient.forceUpdate();
-  }
 
-  espClient.setX509Time(timeClient.getEpochTime());
 
-}
+    server = smtplib.SMTP ('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(sender_email, password)
+    server.sendmail (sender_email, rec_email, m)
+    print(" email.has been sent to" ,rec_email)
+@app.route("/")
+def index():
+    return render_template("index.html")
 
-void reconnect() {
-  // Loop until we're reconnected
-  while (!client.connected()) {
-    Serial.print("Attempting MQTT connection...");
-    // Attempt to connect
-    if (client.connect("ESPthing")) {
-      Serial.println("connected");
-      // Once connected, publish an announcement...
-      client.publish("outTopic", "hello world");
-      // ... and resubscribe
-      client.subscribe("inTopic");
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
+@app.route("/recieve_data",methods = ['POST'])
+def recieve_data():
+    # data = request.get_json()
+    # print(data)
+    date = request.values.get('date')
+    entered_amount = request.values.get('entered_amount','0.0')
+    tid_s=''
+    v_id='Vendor 1'
+    for i in range(0, 500, 1):
+        tid = uuid.uuid4()
+        tid = str(tid)
+        tid_s = tid
+        print(tid)
+    res = petrol_table.put_item(
+    Item={
+            'Date': date,
+            'TID': tid,
+            'Quantity': Decimal(entered_amount),
+            'VID':v_id,
+            'Email': request.values.get("email")
+            
+        }
+    )
+    mydb = mysql.connector.connect(
+        host='127.0.0.1', user='root', password='', database='petrol')
+    mycursor = mydb.cursor()
+    insert_stmt = (
+        "INSERT INTO petrol_data(date, tid, vid, amount, email)"
+        "VALUES (%s, %s, %s, %s, %s)"
+        )
+    data = ( date, tid, v_id, Decimal(entered_amount), request.values.get("email"))
+    mycursor.execute(insert_stmt, data)
+    mydb.commit()
+        
+    sendEmail(request.values.get("email"),date,entered_amount,tid_s,v_id)
 
-      char buf[256];
-      espClient.getLastSSLError(buf, 256);
-      Serial.print("WiFiClientSecure SSL error: ");
-      Serial.println(buf);
+    print(date,entered_amount)
+    
+    return str(res),200
+#==========================================================================
 
-      // Wait 5 seconds before retrying
-      delay(5000);
-    }
-  }
-}
 
-void setup() {
 
-  Serial.begin(115200);
-  Serial.setDebugOutput(true);
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
-  setup_wifi();
-  delay(1000);
-  if (!SPIFFS.begin()) {
-    Serial.println("Failed to mount file system");
-    return;
-  }
 
-  Serial.print("Heap: "); Serial.println(ESP.getFreeHeap());
+@app.route("/get_sum",methods=["POST"])
+def get_sum():
+     date = request.get_json()
+     
+     response = petrol_table.query(
+        KeyConditionExpression=Key('Date').eq(date.get("date"))
+     )
+     res = response['Items']
 
-  // Load certificate file
-  File cert = SPIFFS.open("/cert.der", "r"); //replace cert.crt eith your uploaded file name
-  if (!cert) {
-    Serial.println("Failed to open cert file");
-  }
-  else
-    Serial.println("Success to open cert file");
+     sum=0;
+    
+     for r in res:
+         sum = sum+ r["Quantity"]
 
-  delay(1000);
+     
+     return str(sum),200
+    
+                    
+@app.route("/get",methods=["POST"])
+def get1():
 
-  if (espClient.loadCertificate(cert))
-    Serial.println("cert loaded");
-  else
-    Serial.println("cert not loaded");
+    r = petrol_table.scan()
+    email = request.get_json().get("email")
+    i=0
+    response_s = []
+    r=r["Items"]
+    for x in r:
+        if 'Email' in x:
+               
+            if x["Email"] == email:
+                x["Quantity"] = float(x["Quantity"])
+                response_s.append( x)
+                i = i+1
+        
+    return jsonify(response_s)
+#==================================================================================================
 
-  // Load private key file
-  File private_key = SPIFFS.open("/private.der", "r"); //replace private eith your uploaded file name
-  if (!private_key) {
-    Serial.println("Failed to open private cert file");
-  }
-  else
-    Serial.println("Success to open private cert file");
+@app.route("/local",methods=["POST"])
+def local():
+    email = request.get_json().get("email")
+    query= "SELECT * FROM petrol_data WHERE email = '"+email+"'"
+    mydb = mysql.connector.connect(
+        host='127.0.0.1', user='root', password='', database='petrol')
+    mycursor = mydb.cursor()
+    r= mycursor.execute(query)
+  ##  sql_select_query = "SELECT * FROM petrol_data WHERE email = 'amirraaj0@gmail.com' "
+    ##mycursor.execute(sql_select_query, (email,))
+    record = mycursor.fetchall()
+    print(record)
+    i=0
+    response_s = []
+   
+    for x in record:
+        response_s.append({
+            "date":x[0],
+            "tid":x[1],
+            "vid":x[2],
+            "amount":float(x[3]),
+            "email":x[4]})
+        
+    return jsonify(response_s)
 
-  delay(1000);
 
-  if (espClient.loadPrivateKey(private_key))
-    Serial.println("private key loaded");
-  else
-    Serial.println("private key not loaded");
 
-  // Load CA file
-  File ca = SPIFFS.open("/ca.der", "r"); //replace ca eith your uploaded file name
-  if (!ca) {
-    Serial.println("Failed to open ca ");
-  }
-  else
-    Serial.println("Success to open ca");
-
-  delay(1000);
-
-  if (espClient.loadCACert(ca))
-    Serial.println("ca loaded");
-  else
-    Serial.println("ca failed");
-
-  Serial.print("Heap: "); Serial.println(ESP.getFreeHeap());
-
-  WiFi.macAddress(mac);
-  snprintf(mac_Id, sizeof(mac_Id), "%02x:%02x:%02x:%02x:%02x:%02x",
-           mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
-  Serial.print(mac_Id);
-
-  Serial.println("HX711 scale demo");
-  pinMode(buttonPin, INPUT);
-  scale.begin(DOUT, CLK);
-  scale.set_scale(calibration_factor);
-  scale.tare();
-}
-void loop() {
-
-  if (!client.connected()) {
-    reconnect();
-  }
-  client.loop();
-
-  long now = millis();
-  if (now - lastMsg > 2000) {
-    lastMsg = now;
-    //============================================================================
-    float a = scale.get_units(3);
-    Serial.print(a, 3);
-    Serial.print(" litre      ");
-    buttonState = digitalRead(buttonPin);
-
-    //snprintf (msg, BUFFER_LEN, "{\"mac_Id\" : \"%s\", \"random_number\" : %d, \"random_string\" : \"%s\"}", macIdStr.c_str(), randomNumber, randomString.c_str());
-
-    String macIdStr = mac_Id;
-    //uint8_t randomNumber = random(20, 50);
-    //String randomString = String(random(0xffff), HEX);
-
-    if (buttonState == HIGH)
-    {
-      float i = a / 0.400;
-      buttonState = digitalRead(buttonPin);
-      snprintf (msg, BUFFER_LEN, "{\"mac_Id\" : \"%s\", \"Density\" : \"%f\"}", macIdStr.c_str(), i);
-      digitalWrite(buttonPin, LOW);
-      delay(200);
-      Serial.print("Publish message: ");
-      Serial.println(msg);
-      //mqttClient.publish("outTopic", msg);
-      client.publish("outTopic", msg);
-    }
 
 
 
     
-    //=============================================================================
-    Serial.print("Heap: "); Serial.println(ESP.getFreeHeap()); //Low heap can cause problems
-  }
-  digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
-  delay(100); // wait for a second
-  digitalWrite(LED_BUILTIN, LOW); // turn the LED off by making the voltage LOW
-  delay(100); // wait for a second
-}
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
